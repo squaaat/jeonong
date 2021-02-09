0a537e7530648b4e436b4a6af388f495408e38ae
DrakeJin <dydwls121200@gmail.com>
2021-02-10 01:09:51 +0900

    fix configuration

--- admin/next.config.js
+++ admin/next.config.js
@@ -1,6 +1,8 @@
-module.exports = {
-  env: {
-    J_ENV: process.env.J_ENV,
-    J_CICD: process.env.J_CICD,
+module.exports = function() {
+  return {
+    env: {
+      J_ENV: process.env.J_ENV,
+      J_CICD: process.env.J_CICD,
+    }
   }
 }
