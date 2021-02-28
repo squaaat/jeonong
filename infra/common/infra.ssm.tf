02a1d12a59a410ee81b86bb2cedfa2f02370b7c5
DrakeJin <dydwls121200@gmail.com>
2021-03-01 03:04:39 +0900

    fix: don't use arn value in serverless.yml code

--- infra/common/infra.ssm.tf
+++ infra/common/infra.ssm.tf
@@ -14,6 +14,13 @@ resource "aws_ssm_parameter" "aws_acm_certificate_nearsfeed_com_domain" {
   overwrite = true
 }
 
+resource "aws_ssm_parameter" "aws_acm_certificate_nearsfeed_com_arn" {
+  name      = "/nearsfeed/infra/aws_acm_certificate/nearsfeed.com/arn"
+  type      = "String"
+  value     = data.aws_acm_certificate.nearsfeed_com.arn
+  overwrite = true
+}
+
 data "aws_acm_certificate" "nearsfeed_com" {
   domain   = "nearsfeed.com"
   statuses = ["ISSUED"]
@@ -23,5 +30,6 @@ output "infra_ssm" {
   value = {
     aws_route_53_nearsfeed_com_zone_id       = aws_ssm_parameter.aws_route_53_nearsfeed_com_zone_id.value
     aws_acm_certificate_nearsfeed_com_domain = aws_ssm_parameter.aws_acm_certificate_nearsfeed_com_domain.value
+    aws_acm_certificate_nearsfeed_com_arn    = aws_ssm_parameter.aws_acm_certificate_nearsfeed_com_arn.value
   }
 }
